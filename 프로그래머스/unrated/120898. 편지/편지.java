093520a399fb8137a733c1ab8d61a81af0ca4a89
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-10-30 21:27:58 +0900

    Update 편지.java

--- 프로그래머스/unrated/120898. 편지/편지.java
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String message) {
         int answer = 0;
-        String[] div = message.split("");
+        String[] div = message.split(""); // split을 안해도 공백까지 length에 포함됨.....
        
         return div.length*2;
     }
