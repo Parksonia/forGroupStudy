61a5b991345d4d59c5bd0f24a33cf77325e4b669
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-02 12:59:48 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int r = n%7;
+        if (r==0) { 
+        return n/7;
+       }else if(r!=0) {
+       if(n<7) {
+        return 1;
+       } else{
+        return n/7+1;
+       }
+    }
+        return answer;
+    }
+}
