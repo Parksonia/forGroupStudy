604df484de447a11bf5ded4f74e6ab6ebc98bdc3
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-04 18:36:54 +0900

    Update 영수증.java

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -21,3 +21,31 @@ public class Main{
         
     }
 }
+/*
+런타임에러 
+import java.io.*;
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //BufferedReader는 String 으로만 읽고 가져옴
+        int x = Integer.parseInt(br.readLine()); // 숫자형으로 형변환하여 입력받기
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i=0; i<n; i++){
+            int a = Integer.parseInt(br.readLine());
+            int b = Integer.parseInt(br.readLine());
+            x = x-(a*b);
+        }
+        if(x == 0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
+*/
