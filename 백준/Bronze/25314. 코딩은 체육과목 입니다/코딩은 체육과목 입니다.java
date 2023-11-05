634211097d87765df25bd6306ff2b8530a148a13
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-05 18:11:38 +0900

    Update 코딩은 체육과목 입니다.java

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -13,3 +13,21 @@ public class Main {
         System.out.println(answer+"int");
     }
 }
+/*
+String  + 연산은 지양 해야 함 
+import java.io.*;
+
+public class Main {
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine())/4;
+        String answer ="";
+        
+        for(int i=0; i<n; i++){
+            answer+="long ";
+        }   
+        System.out.println(answer+"int");
+    }
+}
+*/
