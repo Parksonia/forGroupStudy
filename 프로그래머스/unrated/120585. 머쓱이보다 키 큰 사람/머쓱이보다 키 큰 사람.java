9cb08be87c1a405f030d11539052997759f39e4c
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-06 11:24:01 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -3,13 +3,21 @@ class Solution {
         int answer = 0;
         int cnt =0;
         
+     
+        for(int i : array) {
+            if(i>height){
+                cnt+=1;
+            }
+        }
+        answer = cnt;
+      /*  
         for(int i=0; i<array.length; i++) {
             if(array[i] >height) {
                 cnt+=1;
             }
             answer=cnt;
         }
-        
+        */
         return answer;
     }
 }
