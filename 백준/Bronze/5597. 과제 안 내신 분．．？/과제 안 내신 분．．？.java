2b11886d06aab4649ebdc509ae66bc140433bb4d
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-13 13:48:29 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,26 +1,23 @@
-/*
-스스로 해결 못함
-1.boolean type 으로 해결할 생각 못함
-2. 간단한 방법으로 hashset 으로 .remove() .contains() 를 활용한 방법도 있음 
-*/
-
-
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean[] submitted = new boolean[30];
-
-        for (int i = 0; i < 28; i++) {
-            int num = sc.nextInt();
-            submitted[num - 1] = true;
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        boolean[] check = new boolean[31]; //1~30번 출석체크
+        //int[]check = new int[31]; int형으로 찾아도 됨
+        
+        for(int i=0; i< 28; i++) { 
+        int num = Integer.parseInt(br.readLine()); //28번 번호 입력
+        check[num - 1] = true; //출첵 완 번호는 1-30번이고 인덱스니까 -1
         }
-
-        for (int i = 0; i < 30; i++) {
-            if (!submitted[i]) {
-                System.out.println(i + 1);
-            }
+        
+        for(int i=0; i<30; i++){
+            if(!check[i]) {  //false면
+                System.out.println(i+1);
+            }  
         }
+        
+        
     }
 }
