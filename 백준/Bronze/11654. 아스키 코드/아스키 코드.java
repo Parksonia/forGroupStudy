f498998f80c5fefd9178fb2370272f2f5b348ee6
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-15 02:04:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[]args) {
+        Scanner sc =new Scanner(System.in);
+         char input = sc.next().charAt(0);
+         int num = (int)input;
+        System.out.println(num);
+    }
+}
