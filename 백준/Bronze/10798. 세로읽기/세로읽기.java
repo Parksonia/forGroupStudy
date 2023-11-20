c402096e05cebb4420fb18588e5e33972346abbb
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-11-21 02:00:01 +0900

    [Bronze I] Title: 세로읽기, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            String input = sc.nextLine();
+            list.add(input);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int maxLength = 0;
+        for (String word : list) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+
+        for (int i = 0; i < maxLength; i++) {
+            for (String word : list) {
+                if (i < word.length() && word.charAt(i) != ' ') {
+                    sb.append(word.charAt(i));
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
