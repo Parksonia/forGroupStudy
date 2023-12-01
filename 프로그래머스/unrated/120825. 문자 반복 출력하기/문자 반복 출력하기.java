229002c7b0611977857f261a48894a29293dd6a7
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-12-01 21:16:30 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -12,3 +12,21 @@ class Solution {
         return answer;
     }
 }
+
+
+/*
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] str = my_string.split("");
+        for(int i=0; i<my_string.length(); i++){
+            answer += str[i].repeat(n);  //String 의 메서드로 .repeat(int)반복 메서드를 활용하는 방법도 있음 파라미터로 주어진 횟수만큼 반복함
+        }
+        return answer;
+    }
+}
+
+
+
+*/
