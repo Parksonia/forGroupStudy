25b6a3945fe3dfd2723ca2bdc54226625a8c7d8e
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-12-07 03:28:53 +0900

    [level unrated] Title: 모음 제거, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String [] arr = {"a","e","i","o","u"};
+        
+       
+        for (int i = 0; i < arr.length; i++) {
+            if (my_string.contains(arr[i])) {
+                my_string = my_string.replace(arr[i], "");
+            }
+        }
+
+        return my_string;
+    }
+}
