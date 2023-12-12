a92ba338d82bcc23ce47d4d907be6221ad92267b
Parksonia <140988009+Parksonia@users.noreply.github.com>
2023-12-12 16:54:30 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.39 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string = my_string.toLowerCase();
+        char [] transfer = my_string.toCharArray();
+        Arrays.sort(transfer);
+        answer = String.valueOf(transfer);  //char[] 를 바로 String 으로 형변환
+        
+        
+        return answer;
+    }
+}
