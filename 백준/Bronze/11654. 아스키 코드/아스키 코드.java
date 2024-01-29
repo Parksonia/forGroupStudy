cb7916276875f0ed858b53cd0c7b1144d6925569
Parksonia <140988009+Parksonia@users.noreply.github.com>
2024-01-29 23:52:46 +0900

    Update 아스키 코드.java

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -2,9 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main (String[]args) {
+       
         Scanner sc =new Scanner(System.in);
-         char input = sc.next().charAt(0);
-         int num = (int)input;
+        
+        char input = sc.next().charAt(0);
+         
+        int num = (int)input;
+       
         System.out.println(num);
     }
 }
