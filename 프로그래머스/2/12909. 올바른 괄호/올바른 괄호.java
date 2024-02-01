be17e9a1bcdc5f26b7f4fb99cbb96eae8f88e1e2
Parksonia <140988009+Parksonia@users.noreply.github.com>
2024-02-01 23:48:23 +0900

    [level 2] Title: 올바른 괄호, Time: 19.95 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='(') {
+               st.push(s.charAt(i)) ;
+            }else if(s.charAt(i) ==')'){
+                if(st.isEmpty() || st.pop() !='('){
+                    answer = false;
+                }
+            }
+        }
+        if(!st.isEmpty()) {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
+
