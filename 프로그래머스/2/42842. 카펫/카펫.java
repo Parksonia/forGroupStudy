f0a5f9470288e53ce463f69fce9f1466609273a9
Parksonia <140988009+Parksonia@users.noreply.github.com>
2024-02-02 22:56:05 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int [2];
+        
+        
+        //테두리는 한줄만 갈색이어야함 !!!! 주의!! 테두리(갈색)를 뺀 내부영역의 크기 => (a-2)(b-2)
+        int size = brown+yellow;
+        int a = 0;
+        int b = 0;
+
+        for(int i=1; i<= size; i++) {
+             if(size%i == 0 ){
+             a = i;
+             b = size/i;           
+             }
+     
+            if((a-2)*(b-2)==yellow){ 
+                answer[0] = a;
+                answer[1] = b;    
+            }
+    
+        }
+    
+        return answer;
+    }
+}
